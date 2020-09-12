f8b56eaaa198dcf767681338ee418ffe28f096d4
Paolo Ramos <paolorr@gmail.com>
2020-09-12 16:39:32 -0300

    Scroll to top on every router transition

--- src/App/routes.tsx
+++ src/App/routes.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useLayoutEffect } from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 import Home from '../Home';
 import Checkout from '../Checkout';
 
-const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/checkout" exact component={Checkout} />
-    <Route>
-      <h1>Page not found</h1>
-    </Route>
-  </Switch>
-);
+const Routes: React.FC = () => {
+  const { pathname, search, hash } = useLocation();
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname, search, hash]);
+
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/checkout" exact component={Checkout} />
+      <Route>
+        <h1>Page not found</h1>
+      </Route>
+    </Switch>
+  );
+};
 
 export default Routes;
